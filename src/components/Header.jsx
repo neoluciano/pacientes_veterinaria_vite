02ce769492da5420d29183b5297f37f6d8f4ca4e
neoluciano <luciano.pereyra@gmail.com>
2023-09-30 14:44:43 -0300

    Commit en master

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header (){
 
     return (
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Seguimiento Pacientes 
+            Seguimiento Pacientes
             <span className=" text-indigo-600 ml-2">Veterinaria</span>
         </h1>
     )
